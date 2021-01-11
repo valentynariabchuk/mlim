648af9e398ba9539a006f2de767c7aeb05c13f98
Sebastian Gabel <1983720+sbstn-gbl@users.noreply.github.com>
2021-01-11 22:48:34 +0100

    Week 08 (#16)

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,13 +1,17 @@
 # create virtualenv and install Python modules
 # change paths if you want
 
-PATH_ENV=$HOME/env
+PATH_ENV=$HOME/env-mlim
 PATH_REPO=$HOME/repos/mlim
 
 python3 -m venv $PATH_ENV
+
 source $PATH_ENV/bin/activate
-
-pip install --upgrade pip
+pup install --upgrade pip
 pip install -r $PATH_REPO/setup/requirements.txt
+deactivate
 
+source $PATH_ENV/bin/activate
+jupyter serverextension enable --py jupyterlab_code_formatter
+jupyter labextension install jupyterlab-plotly@4.14.1
 deactivate
